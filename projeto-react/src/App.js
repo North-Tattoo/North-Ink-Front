1b727295e18a24b1f5b915261679d01558b70de6
Murilo Morgado <125622403+morgado26@users.noreply.github.com>
2024-03-26 22:52:58 -0300

    criacao do projeto react

---
+++ projeto-react/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { FaRegUserCircle } from "react-icons/fa";
+
+function LandingPage() {
+  return (
+    <>
+      <section className="inicio">
+        <div className="logo">
+          <LogoImagem />
+        </div>
+        <div className="botao-entrar">
+          <FaRegUserCircle />
+          <a href="#">Entrar</a>
+        </div>
+        <header className="frase-titulo">
+          ENCONTRE O SEU <span style={{ color: '#9333EA' }}>TATUADOR</span> IDEAL.
+        </header>
+        <article className="subtitulo">
+          Na nossa plataforma, <span style={{ color: '#9333EA' }}>TATUADOR</span> conectamos você aos melhores tatuadores em
+          minutos, prontos para transformar suas
+          ideias em tinta. Explore estilos únicos e faça da sua próxima tatuagem uma <span style={{ color: '#9333EA' }}>TATUADOR</span> inesquecível.
+        </article>
+        <div className="botoes-inicio">
+          <button style={{ backgroundColor: '#9333EA' }}>SEJA UM PARCEIRO</button>
+          <button style={{ backgroundColor: '#5B5B5B' }}>BUSCAR PROFISSIONAIS</button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+// Nossa logo componentizada:
+
+function LogoImagem() {
+  return (
+    <img src="assets/logo-branca.png" alt="" />
+  )
+}
+
+export default LandingPage;
