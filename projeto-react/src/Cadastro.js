e8f1ed29ff67fa34973ab9563d4604fa48dfc950
Murilo Morgado <125622403+morgado26@users.noreply.github.com>
2024-04-01 21:11:20 -0300

    primeira tela de cadastro em html

---
+++ projeto-react/src/Cadastro.js
@@ -0,0 +1,10 @@
+import Cadastro from './Cadastro/Cadastro';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('cadastro'));
+root.render(
+  <React.StrictMode>
+    <Cadastro />
+  </React.StrictMode>
+);
---
+++ projeto-react/src/Cadastro/Cadastro.js
@@ -0,0 +1,9 @@
+// function Cadastro(){
+//     return (
+//         <>
+//         <h1>oiiiiiiiiii</h1>
+//         </>
+//     )
+// }
+
+// export default Cadastro;
