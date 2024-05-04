779ab346d13e7f5ee4015831fcff4db061f8be1b
Sophia Amaral <126505648+sophiaamaral@users.noreply.github.com>
2024-05-04 11:26:39 -0300

    adicionando-rota-listagem

--- north-vite/src/components/cardListagem/GridListagem.jsx
+++ north-vite/src/components/cardListagem/GridListagem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './GridListagem.module.css';
 import studioTatuagens from '../../utils/assets/tatuagem-grid-card.jpg'
 import fotoPerfilGrid from '../../utils/assets/tatuador-grid.png'
+import { Link } from 'react-router-dom';
 import seta from '../../utils/assets/stea.png'
 import { IoIosArrowForward } from "react-icons/io";
 
@@ -20,6 +21,7 @@ function GridListagem() {
             <img className={`${styles.imagem} imagem3`} src="https://img.freepik.com/fotos-gratis/uma-mao-tatuada-de-mulher-segura-decoracao-de-flores-gerada-por-ia_188544-45554.jpg?t=st=1712936410~exp=1712940010~hmac=b6440133274260c03f7a2d3932e833553879dfe8c22c52e9b3f7262d1600e867&w=1060" alt="" />
           </div>
         </div>
+        <Link to='/detalhes'>
         <div className={styles.pointerSombra}>
           <div className={styles.informacoesStudio}>
             <img className={styles.fotoPerfil} src={fotoPerfilGrid} alt="" />
@@ -27,10 +29,12 @@ function GridListagem() {
               <h5>King Tatto</h5>
               <p>Rua Ficticia, 1234 - Ficção</p>
             </div>
+            
             <IoIosArrowForward size={32}/>
           </div>
           <p className={styles.taxaHoraria}>Taxa Horária: R$ 100,00</p>
         </div>
+        </Link>
         <div className={styles.iconeContainer}>
           {/* Ícones com texto */}
           <div className={styles.icone}>Traço Fino</div>
@@ -38,9 +42,11 @@ function GridListagem() {
           <div className={styles.icone}>Encobrimentos</div>
           <div className={styles.icone}>Tinta Vegana</div>
         </div>
+        <Link to='/detalhes'>
         <div className={styles.botaoTenhoInteresse}>
           <button className={styles.tenhoInteresse}>Ver Portifólio</button>
         </div>
+        </Link>
       </div>
     </div>
   );
