abf24a938071599c598e08c51ae76eb2fadb546a
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2024-05-06 09:35:07 -0300

    parse commit

---
+++ projeto-north-ink/src/main/java/school/sptech/northink/projetonorthink/api/util/GerenciadorDeArquivoCSV.java
@@ -0,0 +1,105 @@
+package school.sptech.northink.projetonorthink.api.util;
+
+import school.sptech.northink.projetonorthink.domain.entity.Usuario;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class GerenciadorDeArquivoCSV {
+    public static void gravaArquivoCsv(ListaObj<Usuario> lista, String nomeArq) {
+        FileWriter arq = null;
+        Formatter saida = null;
+        boolean deuRuim = false;
+
+        nomeArq += "Usuarios.csv";
+
+        try {
+            arq = new FileWriter(nomeArq);
+            saida = new Formatter(arq);
+        } catch (IOException erro) {
+            System.out.println("Erro ao abrir o arquivo");
+            System.exit(1);
+        }
+
+        try {
+            for (int i = 0; i < lista.getTamanho(); i++) {
+
+                Usuario usuario = lista.getElemento(i);
+                saida.format("%d;%s;%s;%.2f\n",
+                        usuario.getId(),
+                        usuario.getNome(),
+                        usuario.getEmail(),
+                        usuario.getEstilos());
+            }
+        } catch (FormatterClosedException erro) {
+            System.out.println("Erro ao gravar o arquivo");
+            deuRuim = true;
+        } finally {
+            saida.close();
+            try {
+                arq.close();
+            } catch (IOException erro) {
+                System.out.println("Erro ao fechar o arquivo");
+                deuRuim = true;
+            }
+            if (deuRuim) {
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void leArquivoCsv(String nomeArq) {
+        FileReader arq = null;
+        Scanner entrada = null;
+        Boolean deuRuim = false;
+
+        nomeArq += "Usuarios.csv";
+
+        try {
+            arq = new FileReader(nomeArq);
+            entrada = new Scanner(arq).useDelimiter(";|\\n");
+        } catch (FileNotFoundException erro) {
+            System.out.println("Arquivo nao encontrado");
+            System.exit(1);
+        }
+
+        try {
+
+            System.out.printf("%4S %-15S %-9S %6S\n",
+                    "id", "nome","email", "estilo" );
+            while (entrada.hasNext()) {
+
+                Integer id = entrada.nextInt();
+                String nome = entrada.next();
+                String email = entrada.next();
+                String estilo = entrada.next();
+
+                System.out.printf("%4d %-15s %-9s %6.2f\n",
+                        id, nome, email, estilo);
+
+            }
+        } catch (NoSuchElementException erro) {
+            System.out.println("Arquivo com problemas");
+            deuRuim = true;
+        } catch (IllegalStateException erro) {
+            System.out.println("Erro na leitura do arquivo");
+            deuRuim = true;
+        } finally {
+            entrada.close();
+            try {
+                arq.close();
+            } catch (IOException erro) {
+                System.out.println("Erro ao fechar o arquivo");
+                deuRuim = true;
+            }
+            if (deuRuim) {
+                System.exit(1);
+            }
+        }
+    }
+
+
+}
---
+++ projeto-north-ink/src/main/java/school/sptech/northink/projetonorthink/api/util/ListaObj.java
@@ -0,0 +1,102 @@
+package school.sptech.northink.projetonorthink.api.util;
+
+import lombok.Getter;
+
+@Getter
+public class ListaObj <T>{
+
+
+    private T[] vetor;
+    private int nroElem;
+
+    public ListaObj(int tamanho) {
+        vetor = (T[]) new Object[tamanho];
+        nroElem = 0;
+    }
+
+    public void adiciona(T elemento) {
+        if (nroElem >= vetor.length) {
+            System.out.println("Lista está cheia");
+        }
+        else {
+            vetor[nroElem++] = elemento;
+        }
+    }
+
+    public void adiciona(int i, T elemento) {
+        vetor[i] = elemento;
+    }
+
+
+    public void exibe() {
+        if (nroElem == 0) {
+            System.out.println("\nA lista está vazia.");
+        }
+        else {
+            System.out.println("\nElementos da lista:");
+            for (int i = 0; i < nroElem; i++) {
+                System.out.println(vetor[i]);
+            }
+        }
+    }
+
+    public int busca(T elementoBuscado) {
+        for (int i = 0; i < nroElem; i++) {
+            if (vetor[i].equals(elementoBuscado)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean removePeloIndice (int indice) {
+        if (indice < 0 || indice >= nroElem) {
+            System.out.println("\nIndice invalido!");
+            return false;
+        }
+
+        for (int i = indice; i < nroElem-1; i++) {
+            vetor[i] = vetor[i+1];
+        }
+
+        nroElem--;
+        return true;
+    }
+
+    public boolean removeElemento(T elementoARemover) {
+        return removePeloIndice(busca(elementoARemover));
+    }
+
+    public int getTamanho() {
+        return nroElem;
+    }
+
+    public T getElemento(int indice) {
+        if (indice < 0 || indice >= nroElem) {
+            return null;
+        }
+        else {
+            return vetor[indice];
+        }
+    }
+
+
+    public void limpa() {
+        nroElem = 0;
+    }
+
+    public void ordenaPorNome() {
+        for (int i = 0; i < nroElem - 1; i++) {
+            int indMenor = i;
+
+            for (int j = i + 1; j < nroElem; j++) {
+                if (vetor[j].toString().compareTo(vetor[indMenor].toString()) < 0) {
+                    indMenor = j;
+                }
+            }
+            T aux = vetor[i];
+            vetor[i] = vetor[indMenor];
+            vetor[indMenor] = aux;
+        }
+    }
+}
