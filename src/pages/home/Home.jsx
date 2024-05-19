29abd199fd32615a7f1e49ade2d2bd7325ee50f7
Sophia Amaral <126505648+sophiaamaral@users.noreply.github.com>
2024-05-19 20:11:24 -0300

    tela-home-ajustada-footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,10 +6,11 @@ import { Link } from 'react-router-dom';
 import perfilTatuador from '../../utils/assets/perfil.png'
 
 
+
 function Home() {
     return (
         <>
-            <section className={styles["inicio"]}>
+            <div className={styles["inicio"]}>
                 <div className={styles["logo"]}>
                     <LogoImagem />
                 </div>
@@ -26,16 +27,16 @@ function Home() {
                     ideias em tinta. Explore estilos únicos e faça da sua próxima tatuagem uma <span style={{ color: '#9333EA' }}>TATUADOR</span> inesquecível.
                 </article>
                 <div className={styles["botoes-inicio"]}>
-                <Link to='/cadastro'>
-                    <button style={{ backgroundColor: '#9333EA' }}>SEJA UM PARCEIRO</button>
+                    <Link to='/cadastro'>
+                        <button style={{ backgroundColor: '#9333EA' }}>CADASTRE-SE</button>
                     </Link>
                     <Link to='/listagem-tatuadores'>
                         <button style={{ backgroundColor: '#5B5B5B' }}>BUSCAR PROFISSIONAIS</button>
                     </Link>
                 </div>
-            </section>
+            </div>
 
-            <section className={styles["gradiente-site"]}>
+            <div className={styles["gradiente-site"]}>
                 <section className={styles["fotos-tatuagens"]}>
                     <div className={styles["frase-tatuagens"]}>
                         Nós celebramos a <span style={{ color: '#1E1E1E' }}>arte</span> que <br /> você veste. <br />
@@ -58,44 +59,46 @@ function Home() {
                         </div>
                     </div>
                 </section>
-
+            </div>
+            <div className={styles["fundo"]}>
+            <div className={styles["fundo-ondas"]}>
                 <article className={styles["frase-carrossel"]}>Descubra os <span style={{ color: '#1E1E1E' }}>artistas</span> que não apenas ouvem,
-                    <br />
+                    <br/>
                     mas entendem sua <span style={{ color: '#1E1E1E' }}>história</span>.
                 </article>
-
                 <Carrossel />
-
-                <section className={styles["secao-depoimentos"]}>
-                    <header>Na <span style={{ color: '#9333EA' }}>North Ink.</span>, sua melhor escolha! <br />
-                        Profissionais, preço e proximidade em um só lugar.
-                    </header>
-                    <article>Depoimento de um dos nossos <span style={{ color: '#9333EA' }}>parceiros.</span></article>
-                    <div className={styles["depoimento-tatuador"]}>
-                        <img style={{ width: '784px', height: '608px' }} src='https://img.freepik.com/free-photo/experienced-tattoo-artist-front-his-studio_23-2149479274.jpg?w=996&t=st=1714961365~exp=1714961965~hmac=1adfc35390c01de842b35ebf9b36acae743219f48f92bd66ea7c414d0b232030' alt="Depoimento do tatuador" />
-                        <div className={styles["texto-depoimento"]}>
-                            <header>Roberto Otávio de Paula, 45 anos</header>
-                            <article>Proprietário do <span style={{ color: '#9333EA' }}>Tattoo House</span> - Santa Cecilia</article>
-                            <span>
-                                "Desde que me juntei à North Ink, minha carreira decolou! Esta plataforma revolucionária não
-                                apenas
-                                me conectou a uma ampla base de clientes ávidos por minha arte, mas também simplificou todo o
-                                processo. Com a North Ink, tenho a liberdade de me concentrar no que faço de melhor - criar
-                                tatuagens memoráveis - enquanto a plataforma me me aproxima de maneira eficaz do público. É como
-                                ter
-                                um assistente pessoal 24 horas por dia, 7 dias por semana, direcionando clientes de qualidade
-                                até
-                                minha porta. Estou verdadeiramente grato por fazer parte desta comunidade vibrante e por todo o
-                                apoio que recebi da equipe da North Ink. Juntos, estamos transformando sonhos em tinta!"
-                            </span>
-                        </div>
+            </div>
+            <section className={styles["secao-depoimentos"]}>
+                <header>Na <span style={{ color: '#9333EA' }}>North Ink.</span>, sua melhor escolha! <br />
+                    Profissionais, preço e proximidade em um só lugar.
+                </header>
+                <article>Depoimento de um dos nossos <span style={{ color: '#9333EA' }}>parceiros.</span></article>
+                <div className={styles["depoimento-tatuador"]}>
+                    <img style={{ width: '784px', height: '608px' }} src='https://img.freepik.com/free-photo/experienced-tattoo-artist-front-his-studio_23-2149479274.jpg?w=996&t=st=1714961365~exp=1714961965~hmac=1adfc35390c01de842b35ebf9b36acae743219f48f92bd66ea7c414d0b232030' alt="Depoimento do tatuador" />
+                    <div className={styles["texto-depoimento"]}>
+                        <header>Roberto Otávio de Paula, 45 anos</header>
+                        <article>Proprietário do <span style={{ color: '#9333EA' }}>Tattoo House</span> - Santa Cecilia</article>
+                        <span>
+                            "Desde que me juntei à North Ink, minha carreira decolou! Esta plataforma revolucionária não
+                            apenas
+                            me conectou a uma ampla base de clientes ávidos por minha arte, mas também simplificou todo o
+                            processo. Com a North Ink, tenho a liberdade de me concentrar no que faço de melhor - criar
+                            tatuagens memoráveis - enquanto a plataforma me me aproxima de maneira eficaz do público. É como
+                            ter
+                            um assistente pessoal 24 horas por dia, 7 dias por semana, direcionando clientes de qualidade
+                            até
+                            minha porta. Estou verdadeiramente grato por fazer parte desta comunidade vibrante e por todo o
+                            apoio que recebi da equipe da North Ink. Juntos, estamos transformando sonhos em tinta!"
+                        </span>
                     </div>
-                </section>
-
-                <div className={styles["logo-final-do-site"]}>
-                    <img src='\src\utils\assets\logo-sem-nome.png' alt="Logo final do site" />
                 </div>
             </section>
+
+            <div className={styles["logo-final-do-site"]}>
+                <img src='\src\utils\assets\logo-sem-nome.png' alt="Logo final do site" />
+            </div>
+            </div>
+
 
             <Footer />
         </>
