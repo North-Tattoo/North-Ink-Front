2f926ba40626325d334d510fa569fc9088f04006
Murilo Morgado <muri.sampaio26@gmail.com>
2024-05-27 20:59:10 -0300

    tela de gerenciamento do estúdio

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Sidebar,
   Menu,
   MenuItem,
-  SubMenu,
   useProSidebar,
 } from "react-pro-sidebar";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
@@ -12,70 +11,86 @@ import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
 import HomeIcon from "@mui/icons-material/Home";
 import InsertChartIcon from "@mui/icons-material/InsertChart";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Sidebar.module.css";
-import { FaHome } from "react-icons/fa";
 
 function SidebarGerenciamentoConta() {
   const { collapseSidebar } = useProSidebar();
+  const location = useLocation();
+  const path = location.pathname;
+
+  // Define a altura da sidebar com base no caminho atual
+  let height;
+  switch (path) {
+    case '/gerenciamento-conta/perfil':
+      height = "100vh"; // altura para o perfil
+      break;
+    case '/gerenciamento-conta/portfolio':
+      height = "150vh"; // altura para o portfólio
+      break;
+    case '/gerenciamento-conta/estudio':
+      height = "130vh"; // altura para o estúdio
+      break;
+    default:
+      height = "100vh"; // Altura padrão
+  }
 
   return (
-    <>
-      <div style={{ display: "flex", height: "100vh" }}>
-        <Sidebar backgroundColor="#3C3C3C" className={styles["app"]}>
-          <Menu
-            menuItemStyles={{
-              button: {
-                backgroundColor: "#3C3C3C",
+    <div className={styles["container-sidebar"]} style={{ height }}>
+      <Sidebar backgroundColor="#3C3C3C" className={styles["app"]}>
+        <Menu
+          menuItemStyles={{
+            button: {
+              backgroundColor: "#3C3C3C",
+              color: "#fff",
+              marginBottom: "30px",
+              "&:hover": {
+                backgroundColor: "#A855F7",
                 color: "#fff",
-                marginBottom: "30px",
-                "&:hover": {
-                  backgroundColor: "#A855F7",
-                  color: "#fff",
-                },
               },
+            },
+          }}
+        >
+          <MenuItem
+            onClick={() => {
+              collapseSidebar();
             }}
+            className={styles["menu1"]}
+            icon={<MenuRoundedIcon />}
           >
-            <MenuItem
-              onClick={() => {
-                collapseSidebar();
-              }}
-              className={styles["menu1"]}
-              icon={<MenuRoundedIcon />}
-            >
-              <img src="..\src\utils\assets\logo-branca.png" alt="" />
-            </MenuItem>
-            <MenuItem
-              component={<Link to="/gerenciamento-conta/perfil" />}
-              icon={<PersonIcon />}
-            >
-              Meu Perfil
-            </MenuItem>
-            <MenuItem
-              component={<Link to="/gerenciamento-conta/portfolio" />}
-              icon={<FeaturedPlayListIcon />}
-            >
-              Portfólio
-            </MenuItem>
-            <MenuItem
-              component={<Link to="/gerenciamento-conta/estudio" />}
-              icon={<HomeIcon />}
-            >
-              Estúdio
-            </MenuItem>
-            <MenuItem
-              component={<Link to="destaques" />}
-              icon={<InsertChartIcon />}
-            >
-              Destaques
-            </MenuItem>
-            <MenuItem component={<Link to="logout" />} icon={<LogoutIcon />}>
-              Sair
-            </MenuItem>
-          </Menu>
-        </Sidebar>        
-      </div>
-    </>
+            <img src="..\src\utils\assets\logo-branca.png" alt="" />
+          </MenuItem>
+          <MenuItem
+            component={<Link to="/gerenciamento-conta/perfil" />}
+            icon={<PersonIcon />}
+          >
+            Meu Perfil
+          </MenuItem>
+          <MenuItem
+            component={<Link to="/gerenciamento-conta/portfolio" />}
+            icon={<FeaturedPlayListIcon />}
+          >
+            Portfólio
+          </MenuItem>
+          <MenuItem
+            component={<Link to="/gerenciamento-conta/estudio" />}
+            icon={<HomeIcon />}
+          >
+            Estúdio
+          </MenuItem>
+          <MenuItem
+            component={<Link to="destaques" />}
+            icon={<InsertChartIcon />}
+          >
+            Destaques
+          </MenuItem>
+          <MenuItem component={<Link to="logout" />} icon={<LogoutIcon />}>
+            Sair
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
   );
 }
+
 export default SidebarGerenciamentoConta;
--- src/pages/gerenciamento/GerenciamentoEstudio/GerenciamentoEstudio.jsx
+++ src/pages/gerenciamento/GerenciamentoEstudio/GerenciamentoEstudio.jsx
@@ -1,15 +1,130 @@
 import React from "react";
 import SidebarGerenciamentoConta from "@/components/sidebar/Sidebar";
+import styles from "./GerenciamentoEstudio.module.css";
+import { useForm } from "react-hook-form";
+import { FaSearch } from "react-icons/fa";
 
 function Estudio() {
+  const { register, handleSubmit, setValue, watch } = useForm();
+
+  const checkCEP = (data) => {
+    const cep = watch('cep').replace(/\D/g, '');
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
+      console.log(data);
+      setValue('estado', data.uf);
+      setValue('cidade', data.localidade);
+      setValue('bairro', data.bairro);
+      setValue('rua', data.logradouro);
+      setValue('numero', ''); // Clear address number field
+      setValue('complemento', ''); // Clear address complement field
+    });
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit(checkCEP)();
+    }
+  };
+
   return (
-      <div style={{display: "flex"}}>
-        <SidebarGerenciamentoConta />
+    <div style={{ display: "flex" }}>
+      <SidebarGerenciamentoConta />
+      <section className={styles["fora-da-sidebar"]}>
+        <h1 className={styles["titulo"]}>Estúdio.</h1>
+        <h3>Edite seu estúdio.</h3>
+
         <div>
-            <h1>Estudio PAGE</h1>
-            <h3>Welcome User</h3>
-            <p>Lorem ipsum dolor sit amet...</p>
-       </div>
+          <label htmlFor="nomeEstudio">Nome do estúdio</label>
+          <input id="nomeEstudio" type="text" {...register("nomeEstudio")} />
+
+          <label style={{ marginTop: "10px" }} htmlFor="descricao">
+            Descrição
+          </label>
+          <textarea
+            id="descricao"
+            type="text"
+            placeholder="Conte-nos sobre o seu estúdio"
+            {...register("descricao")}
+          />
+        </div>
+        <h3>Edite seu endereço.</h3>
+
+        <label htmlFor="cep">CEP</label>
+        <div className={styles["ipt-cep"]}>
+          <input
+            style={{ width: "200px" }}
+            id="cep"
+            type="text"
+            {...register("cep")}
+            onKeyPress={handleKeyPress}
+            onBlur={handleSubmit(checkCEP)}
+          />
+          <FaSearch onClick={handleSubmit(checkCEP)} />
+        </div>
+
+        <div className={styles["input-grupo"]}>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="rua">Rua</label>
+            </div>
+            <input id="rua" type="text" {...register("rua")} />
+          </div>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="bairro">Bairro</label>
+            </div>
+            <input
+              style={{ width: "200px" }}
+              id="bairro"
+              type="text"
+              {...register("bairro")}
+            />
+          </div>
+        </div>
+
+        <div className={styles["input-grupo"]}>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="complemento">Complemento</label>
+            </div>
+            <input id="complemento" type="text" {...register("complemento")} />
+          </div>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="numero">Número</label>
+            </div>
+            <input
+              style={{ width: "200px" }}
+              id="numero"
+              type="text"
+              {...register("numero")}
+            />
+          </div>
+        </div>
+
+        <div className={styles["input-grupo"]}>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="estado">Estado</label>
+            </div>
+            <input id="estado" type="text" {...register("estado")} />
+          </div>
+          <div className={styles["campo"]}>
+            <div className={styles["label-container"]}>
+              <label htmlFor="cidade">Cidade</label>
+            </div>
+            <input
+              style={{ width: "200px" }}
+              id="cidade"
+              type="text"
+              {...register("cidade")}
+            />
+          </div>
+        </div>
+
+        <button className={styles["botao-salvar"]}>Salvar</button>
+      </section>
     </div>
   );
 }
