da8fb6a098bbbe915a08e4075ad6f5b1332b04a1
Murilo Morgado <muri.sampaio26@gmail.com>
2024-09-02 13:08:17 -0300

    Tela de benefícios da assinatura

--- site/src/pages/App.jsx
+++ site/src/pages/App.jsx
@@ -11,6 +11,7 @@ import Perfil from "./gerenciamento/GerenciamentoPerfil/GerenciamentoPerfil";
 import Portfolio from "./gerenciamento/GerenciamentoPortfolio/GerenciamentoPortfolio";
 import Estudio from "./gerenciamento/GerenciamentoEstudio/GerenciamentoEstudio";
 import Destaques from "./gerenciamento/GerenciamentoDestaques/GerenciamentoDestaques";
+import Assinatura from "./gerenciamento/assinatura/Assinatura";
 import Pagamento from "./pagamento/Pagamento";
 
 function App(){
@@ -29,7 +30,8 @@ function App(){
               <Route path="/gerenciamento-conta/portfolio" element={<Portfolio />}/>
               <Route path="/gerenciamento-conta/estudio" element={<Estudio />}/>
               <Route path="/gerenciamento-conta/destaques" element={<Destaques />}/>
-              <Route path="/pagamento" element ={<Pagamento/>}/>
+              <Route path="/assinatura" element ={<Assinatura/>}/>
+              <Route path="/pagamento" element={<Pagamento/>}/>
           </Routes>
       </BrowserRouter>
     </ProSidebarProvider>
---
+++ site/src/pages/gerenciamento/assinatura/Assinatura.jsx
@@ -0,0 +1,65 @@
+import SidebarGerenciamentoConta from "@/components/sidebar/Sidebar";
+import styles from "./Assinatura.module.css";
+import { FaCheckCircle } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
+function Assinatura() {
+  const navigate = useNavigate(); 
+
+  function assinar() {
+    console.log('1');
+    
+    navigate('/pagamento');
+
+    console.log('2');
+  }
+
+  return (
+    <div style={{ display: "flex" }}>
+      <SidebarGerenciamentoConta />
+      <section className={styles["fora-da-sidebar"]}>
+        <h1>Destaque seu perfil.</h1>
+        <span>
+          A North Ink valoriza a sua arte! Assine nosso plano exclusivo e
+          destaque-se na página inicial. Com a assinatura, seu perfil será
+          exibido em destaque, garantindo que os clientes te encontrem primeiro
+          quando visitarem nossa plataforma.
+        </span>
+
+        <div className={styles["container-card"]}>
+          <div className={styles.card}>
+            <h2>Tatto Master</h2>
+            <h1>
+              R$ 29,99 <span>/ mês</span>
+            </h1>
+            <hr className={styles.divisor} />
+            <ul>
+              <li>
+                <div className={styles["item"]}>
+                  <FaCheckCircle className={styles["icon-check"]} />
+                  Tenha destaque garantido na tela inicial, aumentando sua
+                  visibilidade.
+                </div>
+              </li>
+              <li>
+                <div className={styles["item"]}>
+                  <FaCheckCircle className={styles["icon-check"]} />
+                  Apareça em primeiro lugar nas pesquisas e atraia mais clientes.
+                </div>
+              </li>
+              <li>
+                <div className={styles["item"]}>
+                  <FaCheckCircle className={styles["icon-check"]} />
+                  Acesse uma dashboard de métricas exclusiva para acompanhar seu desempenho.
+                </div>
+              </li>
+            </ul>
+            <button onClick={assinar} className={styles.botaoAssinar}>ASSINAR</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Assinatura;
