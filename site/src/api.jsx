ff9627f283794bc6897593bb68873ae528c83386
Jeremias Carneiro <jeremias.carneiro@acqio.com.br>
2024-12-07 01:58:24 -0300

    Subindo CI/CD:v1

--- site/src/api.jsx
+++ site/src/api.jsx
@@ -11,14 +11,14 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
     const token = sessionStorage.getItem('token');
     console.log('Token - api.jsx:', token);
-    // console.log('Token:', token);
+   
     if (
         (token && config.method === 'get') || 
         (token && config.method === 'put') || 
         (token && config.method === 'post' && config.url.startsWith('/api/estudios')) || 
         (token && config.method === 'post' && config.url.startsWith('/api/enderecos'))
     ) {
-        console.log('Entrou no if api.jsx');
+        
         config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
